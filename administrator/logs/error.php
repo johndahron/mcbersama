b08458d5193a343a6b9ddf5700d732301b463e83
John Dahron <john.dahron@gmail.com>
2021-11-29 21:20:04 +0700

    Initial commit

---
+++ administrator/logs/error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2021-11-29 07:41:56 UTC
+#Software: Joomla! 4.0.4 Stable [ Furaha ] 24-October-2021 19:25 GMT
+
+#Fields: datetime	priority clientip	category	message
+2021-11-29T07:41:56+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
